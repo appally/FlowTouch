2d6f6e59526b37dadd7ba3580f77fedfd0b73db0
appally <appally@gmail.com>
2025-12-27 12:38:17 +0800

    Initial Commit

---
+++ FlowTouch/FlowTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowTouchApp.swift
+//  FlowTouch
+//
+//  Created by Appally on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct FlowTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
